9c1d9b010c181dad3400b151e0f460475e9783d1
yangdingshan <choxsan@163.com>
2018-10-17 14:14:37 +0800

    代理模式

---
+++ src/main/java/designpatterns/proxy/Proxy.java
@@ -0,0 +1,33 @@
+package designpatterns.proxy;
+
+import designpatterns.decorator.Source;
+import designpatterns.decorator.Sourceable;
+
+/**
+ * @Author: yangdingshan
+ * @Date: 2018/10/17 13:58
+ * @Description: 代理类
+ */
+public class Proxy implements Sourceable {
+
+    private Source source;
+
+    public Proxy() {
+        this.source = new Source();
+    }
+
+    @Override
+    public void menthod() {
+        before();
+        source.menthod();
+        after();
+    }
+
+    private void before() {
+        System.out.println("proxy before");
+    }
+
+    private void after() {
+        System.out.println("proxy after");
+    }
+}
